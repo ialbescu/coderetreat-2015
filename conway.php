dd08f4f3fba40e0d58f05088c23d1439aba3678e
Ioan Albescu <ialbescu@aminte.ro>
2015-11-14 14:34:00 +0200

    end of session 4

--- conway.php
+++ conway.php
@@ -2,5 +2,64 @@
 
 class conway
 {
-	
+	private $cells = [];
+
+	public function getLivingCells()
+	{
+		return [];
+	}
+
+	public function addLivingCell($row, $col)
+	{
+		$this->cells[$row][$col] = 1;
+	}
+
+	public function getCells()
+	{
+		return $this->cells;
+	}
+
+	public function getCellState($row, $col)
+	{
+		if(!isset($this->cells[$row]) || !isset($this->cells[$row][$col]))
+		{
+			return 0;
+		}
+
+		return $this->cells[$row][$col];
+	}
+
+	public function getCellNeighbours($row, $col)
+	{
+		$numNeigh = 0;
+
+		// get previous line
+		$numNeigh += $this->getCellState($row - 1, $col - 1);
+		$numNeigh += $this->getCellState($row - 1, $col);
+		$numNeigh += $this->getCellState($row - 1, $col + 1);
+
+		// get current line
+		$numNeigh += $this->getCellState($row, $col - 1);
+		$numNeigh += $this->getCellState($row, $col + 1);
+
+		// get next line
+		$numNeigh += $this->getCellState($row + 1, $col - 1);
+		$numNeigh += $this->getCellState($row + 1, $col);
+		$numNeigh += $this->getCellState($row + 1, $col + 1);
+
+		return $numNeigh;
+	}
+
+	public function iterateLivingCells()
+	{
+		$newCells = [];
+
+		for($i = 0; $i < count($this->cells); $i++)
+		{
+			for($j = 0; $j < count($this->cells[$i]); $j++)
+			{
+				// george.m.banica@gmail.com
+			}
+		}
+	}
 }
--- conwayTest.php
+++ conwayTest.php
@@ -10,5 +10,101 @@ include 'conway.php';
 
 class conwayTest extends PHPUnit_Framework_TestCase
 {
+    protected $conway;
 
+    protected function setUp()
+    {
+        $this->conway = new conway();
+    }
+
+    public function testInitializationWithoutLivingCells()
+    {
+        $this->assertEmpty($this->conway->getLivingCells());
+    }
+
+    public function testObjectWithOneLivingCell()
+    {
+        $expected = [
+            0 => [
+                2 => 1
+            ]
+        ];
+
+        $this->conway->addLivingCell(0,2);
+        $this->assertSame($expected, $this->conway->getCells());
+    }
+
+    public function testObjectWithMultipleLivingCell()
+    {
+        $expected = [
+            0 => [
+                2 => 1
+            ],
+            1 => [
+                1 => 1,
+                2 => 1,
+                3 => 1
+            ]
+        ];
+
+        $this->conway->addLivingCell(0,2);
+        $this->conway->addLivingCell(1,1);
+        $this->conway->addLivingCell(1,2);
+        $this->conway->addLivingCell(1,3);
+        $this->assertSame($expected, $this->conway->getCells());
+    }
+
+    public function testLivingCellValues()
+    {
+        $expected = 1;
+        $this->conway->addLivingCell(0,2);
+        $this->assertEquals($expected, $this->conway->getCellState(0, 2));
+    }
+
+    public function testCellsValues()
+    {
+        $this->conway->addLivingCell(0,2);
+        $this->assertEquals(1, $this->conway->getCellState(0, 2));
+
+        $this->assertEquals(0, $this->conway->getCellState(0, 0));
+    }
+
+    public function testCellNeighbours()
+    {
+        $this->conway->addLivingCell(0,2);
+        $this->conway->addLivingCell(1,1);
+        $this->conway->addLivingCell(1,2);
+        $this->conway->addLivingCell(1,3);
+
+        $this->assertEquals(3, $this->conway->getCellNeighbours(0,3));
+    }
+
+    public function testInterate()
+    {
+        $this->conway->addLivingCell(0,2);
+        $this->conway->addLivingCell(1,1);
+        $this->conway->addLivingCell(1,2);
+        $this->conway->addLivingCell(1,3);
+
+        $this->conway->iterateLivingCells();
+
+        $expected = [
+            0 => [
+                1 => 1,
+                2 => 1,
+                3 => 1,
+            ],
+            1 => [
+                1 => 1,
+                2 => 1,
+                3 => 1,
+            ],
+            2 => [
+                2 => 1
+            ]
+        ];
+
+        $this->assertSame($expected, $this->conway->getCells());
+
+    }
 }
